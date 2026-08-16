66ebaf91498864073d9d4f03d0cd0794c62bdae5
Maksim Kainov <@email.com>
2026-08-16 14:36:24 +0300

    Final ready test9

--- bot.py
+++ bot.py
@@ -166,7 +166,9 @@ ABOUT_TEXT = """Привет! 👋
 
 SUPPORT_TEXT = """Если возникли вопросы — напишите мне напрямую: @veranikkiri"""
 
-TRIAL_TEXT = """🎁 <b>Пробная тренировка 🎁</b>\n\nПрактика ждет тебя здесь 🎁https://t.me/yogaloversclub/603""""
+TRIAL_TEXT = """🎁 <b>Пробная тренировка 🎁</b>
+    
+Практика ждет тебя здесь 🎁https://t.me/yogaloversclub/603"""
 
 
 def legal_text(product_key: str) -> str:
